50537ecc3c37c66a41c4010828e97eadcdfe6a3e
SedaQ <pavelseda@email.cz>
2019-06-23 18:14:24 +0200

    Add alternative class.

---
+++ src/main/java/org/gopas/training/persistence/dao/jdbctemplate/PersonDaoJdbcTemplateImpl.java
@@ -0,0 +1,79 @@
+package org.gopas.training.persistence.dao.jdbctemplate;
+
+import java.util.List;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Alternative;
+import org.gopas.training.persistence.dao.PersonDao;
+import org.gopas.training.persistence.model.Person;
+import org.gopas.training.persistence.pojos.PersonIdEmailSurnameCityProjection;
+import org.gopas.training.persistence.pojos.PersonWithContactsProjection;
+
+/**
+ *
+ * @author SedaQ
+ */
+@Alternative
+@RequestScoped
+public class PersonDaoJdbcTemplateImpl implements PersonDao {
+
+    @Override
+    public Person getPersonByIdNamedQuery(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Person> findAllPersons() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Person> findAllPersonsNamedQuery() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Person> findAllNativeQuery() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Person> findAllPersonsWithAddresses() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Person> findAllPersonsInfoWithEntityGraph() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Person> findAllPersonsWithAddressSelectively() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<PersonIdEmailSurnameCityProjection> findAllPersonsEmailSurnameCityProjection() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<PersonWithContactsProjection> findAllPersonsWithContacts() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void persist(Person entity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void remove(Person entity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Person findById(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
